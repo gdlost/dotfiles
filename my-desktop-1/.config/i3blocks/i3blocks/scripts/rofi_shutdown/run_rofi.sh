05cdeb65c2365e280716f8f0ee419c46e1312a63
gdlost <juan@juan-y530.localdomain>
2020-09-05 12:04:15 -0400

    ver 1

--- my-desktop-1/.config/i3blocks/i3blocks/scripts/rofi_shutdown/run_rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-~/.config/i3blocks/scripts/rofi_shutdown//my_menu.sh | \
-        rofi -dmenu -theme ~/.config/i3blocks/scripts/rofi_shutdown/shutdown_menu.rasi -font "serif 32" -u 0
